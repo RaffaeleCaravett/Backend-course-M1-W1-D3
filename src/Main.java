788c2d9d0e23ac36c610f012a5c6b3e02a1902f2
RaffaeleCaravetta <raffaelecaravetta13@gmail.com>
2023-10-04 15:35:55 +0200

    finito primo esercizio

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,25 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
 
-        Rettangolo rettangolo = new Rettangolo(8,5);
+        Rettangolo rettangolo = new Rettangolo(2,8);
+        Rettangolo rettangolo1 = new Rettangolo(5,6);
 
-        System.out.println(rettangolo);
 
-    }
+
+
+
+        System.out.println(rettangolo.stampaRettangolo(2,4));
+        System.out.println(rettangolo.stampaDueRettangoli(rettangolo,rettangolo1));
+
+
+
+
+
+
+
+
+
+
+    System.out.println();
+}
 }
--- src/Rettangoli/Rettangolo.java
+++ src/Rettangoli/Rettangolo.java
@@ -4,32 +4,44 @@ import java.util.Random;
 
 public class Rettangolo {
         Random random = new Random();
-        int id=random.nextInt(1000);
-        int altezza;
-        int larghezza;
-        String figure="Rectangles";
+        private int id=random.nextInt(1000);
+        private int altezza;
+        private int larghezza;
+        private String figure="Rectangles";
         public  Rettangolo(int altezza,int larghezza) {
 if(altezza>0 && larghezza>0){
-    this.altezza=altezza;
-    this.larghezza=larghezza;
-    System.out.println( "Il rettangolo " + this.id + " ha una altezza ed una larghezza rispettivamente di : " +
-        this.altezza + " e " + this.larghezza);
-    System.out.println("L'area di questo rettangolo è : " + calculateArea(altezza,larghezza));
-    System.out.println("Il perimetro di questo rettangolo è : " + calculatePerimeter(altezza,larghezza));
+
+  this.altezza=altezza;
+  this.larghezza=larghezza;
 
 }else{
     System.out.println( "Inserisci dei valori maggiori del numero 0");
 }
         }
 
+    public Rettangolo(Rettangolo rettangolo1,Rettangolo rettangolo2){
+        System.out.println(stampaDueRettangoli(rettangolo1,rettangolo2));
+        System.out.println(rettangolo1.altezza);}
 
-        public int calculateArea (int altezza,int larghezza){
-if(altezza>0&&larghezza>0){
-    return altezza*larghezza;
-}else{
-    return 0;
-}
-        };
+
+
+
+
+
+
+
+
+
+
+
+
+    public int calculateArea (int altezza,int larghezza){
+        if(altezza>0&&larghezza>0){
+            return altezza*larghezza;
+        }else{
+            return 0;
+        }
+    };
     public int calculatePerimeter (int altezza,int larghezza){
         if(altezza>0&&larghezza>0){
             return (2*altezza)+(2*larghezza);
@@ -37,28 +49,53 @@ if(altezza>0&&larghezza>0){
             return 0;
         }
     };
-
-    public int getId() {
-            return this.id;
+    public String stampaRettangolo(int altezza,int larghezza){
+        if(altezza>0&&larghezza>0){
+            int area = altezza * larghezza;
+            int perimetro = (2*altezza)+(2*larghezza);
+            return "L'area del rettangolo è : " +area + "; mentre il perimetro è : " + perimetro;
+        }else{
+            return "Inserisci dei valori positivi sopra lo zero";
         }
-
-        public int getAltezza() {
-            return this.altezza;
-        }
-
-        public void setAltezza(int altezza) {
-            this.altezza = altezza;
-        }
-
-        public int getLarghezza() {
-            return larghezza;
-        }
-
-        public int setLarghezza(int larghezza){
-            return this.larghezza=larghezza;
-        }
+    };
 
 
+    public String stampaDueRettangoli (Rettangolo rettangolo1,Rettangolo rettangolo2){
+
+        int sommaAree= calculateArea(rettangolo1.altezza,rettangolo1.larghezza)+calculateArea(rettangolo2.altezza,rettangolo2.larghezza);
+        int sommaPerimetri= calculatePerimeter(rettangolo1.altezza,rettangolo1.larghezza)+calculatePerimeter(rettangolo2.altezza,rettangolo2.larghezza);
+
+            return "Il perimetro del primo rettangolo è : " + calculatePerimeter(rettangolo1.altezza,rettangolo1.larghezza) + "," +
+                " il perimetro del secondo rettangolo è : " + calculatePerimeter(rettangolo2.altezza,rettangolo2.larghezza) + "." +
+                "L'area del primo rettangolo è : " + calculateArea(rettangolo1.altezza,rettangolo1.larghezza) + "," +
+                "L'area del secondo rettangolo è : " + calculatePerimeter(rettangolo2.altezza,rettangolo2.larghezza) + "." +
+                "La somma delle aree dei due rettangoli è : " + sommaAree
+                + "," +
+                "La somma deli perimetri dei due rettangoli è : " + sommaPerimetri + ".";
 
     }
 
+
+    public int getId() {
+        return this.id;
+    }
+
+    public int getAltezza() {
+        return this.altezza;
+    }
+
+    public void setAltezza(int altezza) {
+        this.altezza = altezza;
+    }
+
+    public int getLarghezza() {
+        return larghezza;
+    }
+
+    public int setLarghezza(int larghezza){
+        return this.larghezza=larghezza;
+    }
+
+
+
+}
